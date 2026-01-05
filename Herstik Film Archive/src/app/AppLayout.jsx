a438e59fca80728dfd93a8f04b0a90e35022b16e
AidanH567 <herstikaidan@gmail.com>
2026-01-05 16:21:14 +0100

    adding routes

---
+++ Herstik Film Archive/src/app/AppLayout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header.jsx';
+
+export default function AppLayout() {
+    return <>;
+        <Header />
+        <main>
+            <Outlet />
+        </main>
+       <Footer/>
+    </>;
+}
---
+++ Herstik Film Archive/src/app/routes.jsx
@@ -0,0 +1,25 @@
+import {Routes, Route} from 'react-router-dom';
+
+import AppLayout from './app/AppLayout.jsx';
+import Home from '../pages/Home.jsx';
+
+import List from '../pages/List.jsx';
+import Journal from '../pages/Journal.jsx';
+import Login from '../pages/Login.jsx';
+import SignUp from '../pages/SignUp.jsx';
+import Reviews from '../pages/Reviews.jsx';
+
+export default function AppRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<AppLayout />}>
+                <Route index element={<Home />} />
+                <Route path="list" element={<List />} />
+                <Route path="journal" element={<Journal />} />
+                <Route path="login" element={<Login />} />
+                <Route path="signup" element={<SignUp />} />
+                <Route path="reviews" element={<Reviews />} />
+            </Route>
+        </Routes>
+    );
+}
